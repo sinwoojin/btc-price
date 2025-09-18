522fc0fb9bd5a3a923802e6c8fff7199babe79c9
sinwoojin <slow3993@gmail.com>
2025-09-18 22:19:17 +0900

    feat: main 페이지 작업중

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,24 @@
+// src/app/(main)/layout.tsx
+
+import Bottom from "@/components/common/Bottom";
+import Footer from "@/components/common/Footer";
+
+import ThemeProvider from "@/lib/utils/theme-context";
+
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ThemeProvider>
+      <div className="flex flex-col min-h-screen bg-gray-900 text-white">
+        <main className="flex-grow container mx-auto p-4 md:p-8">
+          {children}
+        </main>
+        <Bottom />
+        <Footer />
+      </div>
+    </ThemeProvider>
+  );
+}

a826e52f0eebd0a463b4b615ecd35995861396cc
sinwoojin <slow3993@gmail.com>
2025-09-23 21:36:04 +0900

    chore(app): home layout 변경

--- frontend/src/app/(main)/layout.tsx
+++ frontend/src/app/(main)/layout.tsx
@@ -1,24 +1,13 @@
 // src/app/(main)/layout.tsx
 
-import BottomNav from "@/components/common/Bottom";
-import Footer from "@/components/common/Footer";
-
-import ThemeProvider from "@/lib/utils/theme-context";
-
 export default function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ThemeProvider>
-      <div className="flex flex-col min-h-screen bg-gray-900 text-white">
-        <main className="flex-grow container mx-auto p-4 md:p-8">
-          {children}
-        </main>
-        <BottomNav />
-        <Footer />
-      </div>
-    </ThemeProvider>
+    <div className="flex flex-col min-h-screen bg-gray-900 text-white">
+      <main className="flex-grow container mx-auto p-4 md:p-8">{children}</main>
+    </div>
   );
 }

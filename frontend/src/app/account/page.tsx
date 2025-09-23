57717a14806fb82227c6f96c8e950a8a6ea7da77
sinwoojin <slow3993@gmail.com>
2025-09-23 22:54:01 +0900

    feat: 초기설정 재완료

--- frontend/src/app/account/page.tsx
+++ frontend/src/app/account/page.tsx
@@ -1,5 +1,30 @@
+"use client";
+
+import { useUser } from "@/context/AuthUserProvider";
+
 function Account() {
-  return <div>page</div>;
+  const { user } = useUser();
+  const profileMenu = [
+    { name: "Profile", href: "#" },
+    { name: "Settings", href: "#" },
+    { name: "Sign out", href: "#" },
+  ];
+  return (
+    <div className="items-center justify-center p-5">
+      <ol>
+        {profileMenu.map((el, i) => (
+          <li
+            key={i}
+            className="flex flex-col gap-1 items-center justify-center"
+          >
+            {" "}
+            <span>{el.name}</span>
+            <span>{el.href}</span>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 }
 
 export default Account;
---
+++ frontend/src/config/api.ts
@@ -0,0 +1,1 @@
+export const BASE_URL = "http://localhost:4406/api/v1";

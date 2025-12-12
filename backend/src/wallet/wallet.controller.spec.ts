023891a921f5dfe7e787d72dd4175b23091b359f
sinwoojin <slow3993@gmail.com>
2025-12-12 14:15:44 +0900

    test: Add basic unit tests for authentication and wallet controllers and services.

--- backend/src/wallet/wallet.controller.spec.ts
+++ backend/src/wallet/wallet.controller.spec.ts
@@ -1,5 +1,9 @@
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { DatabaseService } from '../database/database.service';
+import { EventsGateway } from '../events/events.gateway';
 import { WalletController } from './wallet.controller';
+import { WalletService } from './wallet.service';
 
 describe('WalletController', () => {
   let controller: WalletController;
@@ -7,6 +11,7 @@ describe('WalletController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [WalletController],
+      providers: [WalletService,{provide: DatabaseService, useValue: {}},{provide: JwtService, useValue: {sign: jest.fn()}},{provide: EventsGateway, useValue: {emit: jest.fn()}}],
     }).compile();
 
     controller = module.get<WalletController>(WalletController);
--- backend/src/wallet/wallet.service.spec.ts
+++ backend/src/wallet/wallet.service.spec.ts
@@ -1,4 +1,7 @@
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { DatabaseService } from '../database/database.service';
+import { EventsGateway } from '../events/events.gateway';
 import { WalletService } from './wallet.service';
 
 describe('WalletService', () => {
@@ -6,7 +9,7 @@ describe('WalletService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [WalletService],
+      providers: [WalletService,{provide: DatabaseService, useValue: {}},{provide: JwtService, useValue: {sign: jest.fn()}},{provide: EventsGateway, useValue: {emit: jest.fn()}}],
     }).compile();
 
     service = module.get<WalletService>(WalletService);
